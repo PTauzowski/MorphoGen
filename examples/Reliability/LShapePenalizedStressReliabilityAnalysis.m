959a5c10c0c5bc5fe9300be19646bef434ebce22
V4shm <nabucco33@tlen.pl>
2023-12-05 15:19:06 +0100

    Penalized stress implementation

--- examples/Reliability/LShapePenalizedStressReliabilityAnalysis.m
+++ examples/Reliability/LShapePenalizedStressReliabilityAnalysis.m
@@ -2,7 +2,7 @@ clear;
 close all;
 l=3;
 c=0.4;
-res = 40;
+res = 20;
 E=210000;
 nu=0.3;
 
@@ -10,23 +10,23 @@ xp=[l 0.4*l];
 P = [0 10];
 
 model = LShapeModelLinear(ShapeFunctionL4,l,res,E,nu,xp,P);
-model.setResultNode([l*0.4 l*0.4]);
+model.setResultNode([0 l]);
 model.plotModel();
 
-randomVariables={RandomVariable("Normal",0,5) RandomVariable("Normal",-10,2)};
+randomVariables={RandomVariable("Normal",-5,0.1) RandomVariable("Normal",-10,1)};
 transform=IndependentTransformation(randomVariables);
 g=loadPenalizedStressPerformanceFunction(model);
 
-% N=5000;
-% mc= MonteCarlo(randomVariables,g,N);
-% [ Pf_mc, p ] = mc.solve();
-% Pf_mc
-% mc.scatterPlots(["Px" "Py"],"Ux");
-% % 
-  hmv = HMV(randomVariables,g,transform,3);
-% form = FORM(randomVariables,g,transform);
-% Pf_form = form.solve()
-% [ Pf, mpp, betar ] = hmv.solve()
+N=5000;
+mc= MonteCarlo(randomVariables,g,N);
+[ Pf_mc, p ] = mc.solve();
+Pf_mc
+mc.scatterPlots(["Px" "Py"],"Ux");
+
+hmv = HMV(randomVariables,g,transform,3);
+form = FORM(randomVariables,g,transform);
+Pf_form = form.solve()
+[ Pf, mpp, betar ] = hmv.solve()
 
 Rfilter = 1.2*l/res;
 penal = 3;
@@ -36,8 +36,8 @@ volFr=0.4;
 topOpt = StressIntensityTopologyOptimizationVol( Rfilter, model.analysis, cutTreshold, penal, volFr, true );
 %[objF, xopt]  = topOpt.solve();
 
-sora = SORA(model,topOpt, hmv);
-[xDet, betaDet, xRel, volDet, volRel] = sora.solve();
-volDet
-volRel
+% sora = SORA(model,topOpt, hmv);
+% [xDet, betaDet, xRel, volDet, volRel] = sora.solve();
+% volDet
+% volRel
 
--- examples/Reliability/loadPenalizedStressPerformanceFunction.m
+++ examples/Reliability/loadPenalizedStressPerformanceFunction.m
@@ -13,9 +13,8 @@ classdef  loadPenalizedStressPerformanceFunction < Function
         function g = computeValue(obj,points)
             g=zeros(size(points,1),1);
             for k=1:size(points,1)
-                ps=obj.model.computeHMstress(210000,0.3,[points(k,1) points(k,2)]);
-                %g(k)=400-sHM;
-                g(k)=600-ps;
+                ps=obj.model.computePenalizedHMstress(210000,0.3,[points(k,1) points(k,2)],8);
+                g(k)=ps-3.5;
             end            
         end
 
