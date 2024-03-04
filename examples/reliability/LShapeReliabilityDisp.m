21ae6bbb560362c8d46cf2a7071af41d4e0c4e7f
V4shm <nabucco33@tlen.pl>
2024-03-04 15:17:36 +0100

    Lshape task example

--- examples/reliability/LShapeReliabilityDisp.m
+++ examples/reliability/LShapeReliabilityDisp.m
@@ -1,68 +1,42 @@
 clear;
 close all;
-l=3;
-c=0.4;
-res = 40;
-E=210000;
-nu=0.3;
 
-xp=[l 0.4*l];
-P = [0 -10];
+l=3; % long edge length
 
-model = LShapeModelLinear(ShapeFunctionL4,l,res,E,nu,xp);
+model = LShapeModelLinear(  ShapeFunctionL4,...
+                             l, ...         % long edge length
+                            40, ...         % short edge resolution
+                            210000, ...     % Young Modulus
+                            0.3, ...        % Poinsson's ratio
+                            [l 0.4*l] ...   % Force location,xp
+     );
+
 model.setResultNode([l l*0.4]);
 model.plotModel();
 
-randomVariables={RandomVariable("Normal",P(1),3) RandomVariable("Normal",P(2),1)};
+randomVariables={RandomVariable("Normal",0,3) RandomVariable("Normal",10,1)};
 transform=IndependentTransformation(randomVariables);
 g=loadPerformanceFunctionDisp(model);
-%gl=loadLinearPerformanceFunction(model,g);
-
-% g.computeValue([1 0])
-% g.computeValue([0 1])
-% 
-% gl.computeValue([1 0])
-% gl.computeValue([0 1])
 
 
-Rfilter = l/res;
-penal = 3;
-cutTreshold = 0.005;
-volFr=0.25;
+topOpt = StressIntensityTopologyOptimization( Rfilter, ...
+            model.analysis, ... % FEM analysis object
+            0.005, ...          % stress intensity treshold for element removal 
+            2, ...              % penalty factor
+            g, ...              % constraint function object
+            true ...            % is finite elements uniform
+ );
 
-topOpt = StressIntensityTopologyOptimizationVol( Rfilter, model.analysis, cutTreshold, penal, volFr, true );
-%topOpt = SIMP_MMA_TopologyOptimizationElasticCompliance(Rfilter, model.analysis, penal, volFr, true);
 [objF, xopt]  = topOpt.solve();
-toc
 
-topOpt.is_silent=true;
+sora2 = SORA('LShapeDispBeta20_2', model,topOpt, randomVariables, g, transform, 2);
+sora3 = SORA('LShapeDispBeta20_3', model,topOpt, randomVariables, g, transform, 3);
+sora4 = SORA('LShapeDispBeta20_4', model,topOpt, randomVariables, g, transform, 4);
+sora5 = SORA('LShapeDispBeta20_5', model,topOpt, randomVariables, g, transform, 5);
 
-% tuner = ReliabilityTaskTuner(model, topOpt, randomVariables, transform, g, 1000000, 2);
-% tic;
-% tuner.tuneMC();
-% toc
-% tuner.plotMCs(["Px" "Py"],'HMstress');
-%tuner.tuneFORM();
-
- sora2 = SORA('LShapeDispBeta20_2', model,topOpt, randomVariables, g, transform, 2);
- sora3 = SORA('LShapeDispBeta20_3', model,topOpt, randomVariables, g, transform, 3);
- sora4 = SORA('LShapeDispBeta20_4', model,topOpt, randomVariables, g, transform, 4);
- sora5 = SORA('LShapeDispBeta20_5', model,topOpt, randomVariables, g, transform, 5);
-
- % sora2.solveX();
- % sora3.solveX();
- %  sora4.solveX();
- % sora5.solveX();
- 
- % topOpt.solve();
-%  sora2.limitReliability();
-  %sora.tabMultiMpp();
-
-% topOpt.solve();
-% sora2.tabReliability();
-% 
-% sora2.checkTuning();
-% sora3.checkTuning();
-   
+sora2.solveX();
+sora3.solveX();
+sora4.solveX();
+sora5.solveX();
 
 
