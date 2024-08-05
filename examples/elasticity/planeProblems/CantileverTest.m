7d1bd01774f0a941836f5ba8bce56dcabf549cf8
piotrek <nabucco33@tlen.pl>
2024-08-05 19:20:16 +0200

    Cantilever test

--- examples/elasticity/planeProblems/CantileverTest.m
+++ examples/elasticity/planeProblems/CantileverTest.m
@@ -15,10 +15,3 @@ model.plotModel();
 model.analysis.plotMaps(["uy" "ux" "sxx" "sxy" "syy" "sHM"],0.1);
 model.fe.plotWired(model.mesh.nodes,model.analysis.qnodal,0.1);
 
-model.setResultNode([l 0]);
-model.computeDisplacement(1,0.3,[0 -1])
-
-model.setResultNode([0 0]);
-model.computeHMstress(1,0.3,[0 -1])
-
-
