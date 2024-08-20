d3d5e525981fd5701ed02556e2e570289a489614
V4shm <nabucco33@tlen.pl>
2024-08-20 15:48:49 +0200

    Symetry in eigenforms

--- examples/elasticity/solidProblems/ColumnTest3D.m
+++ examples/elasticity/solidProblems/ColumnTest3D.m
@@ -1,6 +1,6 @@
 clear;
 close all;
-sf=ShapeFunctionL8;
+sf=ShapeFunctionL27;
 l=1;
 b=0.1;
 h=0.1;
@@ -12,48 +12,50 @@ nu=0.3;
 rho=7850; 
 xp=[b/2 h/2 l];
 Pcr=pi^2*E*J/lw^2;
-P=[0 0 -1];
+
+P=[0 1 0];
 nEigenForms=6;
 %P=[0 1];
 
 model = ColumnModel3D(sf,b,h,l,nl,E,nu,rho,P,xp);
-model.solveWeighted();
-
-model.plotModel();
-model.analysis.plotMaps(["uy" "ux" "sxx" "sxy" "szz" "sHM"],0.1);
-model.fe.plotWired(model.mesh.nodes,model.analysis.qnodal,0.1);
-
-stability=LinearStability(model.analysis.felems, model.mesh);
-stability.Pnodal=model.analysis.Pnodal;
-stability.Pfem=model.analysis.Pfem;
-stability.supports=model.analysis.supports;
-stability.solve(nEigenForms);
-lambdas1=stability.lambdas;
-lambdas1(1,1)
-Pcr
-
-for k=1:min(6,nEigenForms)
-    figure;
-    stability.setForm(k);
-    model.fe.plotSolidDeformed(model.mesh.nodes,stability.qnodal,0.2);
-    title(['Eigen form:' num2str(k)]);
-end
-
-% vibrations=LinearNaturalVibration(model.analysis.felems, model.mesh);
-% vibrations.Pnodal=model.analysis.Pnodal;
-% vibrations.Pfem=model.analysis.Pfem;
-% vibrations.supports=model.analysis.supports;
-% vibrations.solve(nEigenForms);
-% omegas=sqrt(vibrations.lambdas);
-% omegas(1,1)
+% model.solveWeighted();
 % 
+% %model.plotModel();
+% model.analysis.plotMaps(["uy" "ux" "sxx" "sxy" "szz" "sHM"],0.1);
+% model.fe.plotWired(model.mesh.nodes,model.analysis.qnodal,0.1);
+
+% stability=LinearStability(model.analysis.felems, model.mesh);
+% stability.Pnodal=model.analysis.Pnodal;
+% stability.Pfem=model.analysis.Pfem;
+% stability.supports=model.analysis.supports;
+% stability.solve(nEigenForms);
+% lambdas1=stability.lambdas;
+% lambdas1(1,1)
+% Pcr
 % 
 % for k=1:min(6,nEigenForms)
 %     figure;
-%     vibrations.setForm(k);
-%     model.fe.plotWired(model.mesh.nodes,vibrations.qnodal,0.2);
+%     stability.setForm(k);
+%     model.fe.plotSolidDeformed(model.mesh.nodes,stability.qnodal,0.2);
 %     title(['Eigen form:' num2str(k)]);
 % end
+
+vibrations=LinearNaturalVibration(model.analysis.felems, model.mesh);
+vibrations.Pnodal=model.analysis.Pnodal;
+vibrations.Pfem=model.analysis.Pfem;
+vibrations.supports=model.analysis.supports;
+vibrations.solve(nEigenForms);
+omegas=sqrt(vibrations.lambdas);
+3.5160/l^2*sqrt(E*J)
+omegas(1,1)
+
+
+for k=1:min(6,nEigenForms)
+    figure;
+    vibrations.setForm(k);
+    model.fe.plotSolidDeformed(model.mesh.nodes,vibrations.qnodal,0.2);
+    title(['Eigen form:' num2str(k)]);
+end
 
 analysis = SecondOrderElasticityWeighted( model.fe, model.mesh, false );
 %analysis = LinearElasticityWeighted( model.fe, model.mesh, false );
