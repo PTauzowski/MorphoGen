19453b7145994790e9999482a7b973b2fb26829e
BartBlachowski <bblach@ippt.pan.pl>
2024-08-21 09:54:43 +0200

    Further stylistic modifications

--- examples/elasticity/solidProblems/ColumnTest3D.m
+++ examples/elasticity/solidProblems/ColumnTest3D.m
@@ -1,8 +1,9 @@
 clear;
 close all;
 
-sf=ShapeFunctionL27;
-l=1; b=0.1; h=0.1; lw=2*l; nl=50;
+%%% Initialization %%%
+sf=ShapeFunctionL8;
+l=1; b=0.1; h=0.1; lw=2*l; nl=10;
 
 E=205E9; nu=0.3; rho=7850; 
 
@@ -19,77 +20,56 @@ model.solveWeighted();
 % model.analysis.plotMaps(["uy" "ux" "sxx" "sxy" "szz" "sHM"], 0.1);
 % model.fe.plotWired( model.mesh.nodes, model.analysis.qnodal, 0.1);
 
+%%% Buckling analysis %%%
 stability = LinearStability( model.analysis.felems, model.mesh);
 stability.Pnodal = model.analysis.Pnodal;
 stability.Pfem = model.analysis.Pfem;
 stability.supports = model.analysis.supports;
 stability.solve( nEigenForms);
-lambdas1 = stability.lambdas;
-disp(['Eigenvalue = ', num2str(lambdas1(1,1))]),
+lambdas1 = diag(stability.lambdas);
+disp(['Eigenvalue = ', num2str(lambdas1(1))]),
 disp(['P_critical = ', num2str(Pcr)])
 
 for k=1:min( 6, nEigenForms)
     figure;
     stability.setForm(k);
-    model.fe.plotWired( model.mesh.nodes, stability.qnodal, 0.2);
+    model.fe.plotSolidDeformed( model.mesh.nodes, stability.qnodal, 0.2);
+    axis on, xlabel('x-axis'), ylabel('y-axis'), view(3)
     title(['Eigenvector: ' num2str(k)]);
 end
 
-% vibrations=LinearNaturalVibration(model.analysis.felems, model.mesh);
-% vibrations.Pnodal=model.analysis.Pnodal;
-% vibrations.Pfem=model.analysis.Pfem;
-% vibrations.supports=model.analysis.supports;
-% vibrations.solve(nEigenForms);
-% omegas = sqrt( vibrations.lambdas);
-% disp(['Eigenvalue = ', num2str(omegas(1,1))]),
-% disp(['Circular frequency = ', num2str(3.516/l^2*sqrt(E*(b*h^3/12)/(rho*b*h)))])
+%%% Modal analysis %%%
+% vibrations = LinearNaturalVibration(model.analysis.felems, model.mesh);
+% vibrations.Pnodal = model.analysis.Pnodal;
+% vibrations.Pfem = model.analysis.Pfem;
+% vibrations.supports = model.analysis.supports;
+% vibrations.solve( nEigenForms);
+% omegas = sqrt(diag( vibrations.lambdas));
 % 
-% for k=1:min(6,nEigenForms)
-%     figure;
-%     vibrations.setForm(k);
-%     model.fe.plotSolidDeformed(model.mesh.nodes,vibrations.qnodal,0.2);
-%     title(['Mode shape: ' num2str(k)]);
+% disp(['Numerical = ', num2str(omegas(1))])
+% disp(['Analytical = ', num2str(3.5160/l^2*sqrt(E*(b*h^3/12)/(rho*b*h)))])
+% 
+% for k=1:min( 6, nEigenForms)
+%     figure, vibrations.setForm(k);
+%     model.fe.plotSolidDeformed(model.mesh.nodes, vibrations.qnodal, 0.2);
+%     title(['Eigenvector: ' num2str(k)]);
 % end
 
-vibrations=LinearNaturalVibration(model.analysis.felems, model.mesh);
-vibrations.Pnodal=model.analysis.Pnodal;
-vibrations.Pfem=model.analysis.Pfem;
-vibrations.supports=model.analysis.supports;
-vibrations.solve(nEigenForms);
-omegas=sqrt(vibrations.lambdas);
-3.5160/l^2*sqrt(E*J)
-omegas(1,1)
-
-
-for k=1:min(6,nEigenForms)
-    figure;
-    vibrations.setForm(k);
-    model.fe.plotSolidDeformed(model.mesh.nodes,vibrations.qnodal,0.2);
-    title(['Eigen form:' num2str(k)]);
-end
-
-analysis = SecondOrderElasticityWeighted( model.fe, model.mesh, false );
-%analysis = LinearElasticityWeighted( model.fe, model.mesh, false );
-analysis.Pnodal=model.analysis.Pnodal;
-analysis.Pfem=model.analysis.Pfem;
-analysis.supports=model.analysis.supports;
-
-% Filtering radius
-Rfilter = 2*l/nl;
-
-%Removal intensity threshold
-cutTreshold = 0.005;
-
-%penalty factor
-penal = 3;
-
+%%% Topological optimization %%%
+% % Filtering radius
+% Rfilter = 2*l/nl;
+% 
+% %Removal intensity threshold
+% cutTreshold = 0.005;
+% 
+% %penalty factor
+% penal = 3;
 
 % figure;
 % tic
-% topOpt = SIMP_MMA_TopologyOptimizationElasticCompliance(Rfilter, analysis, penal, 0.2, true);
+% topOpt = SIMP_MMA_TopologyOptimizationElasticCompliance( Rfilter, analysis, penal, 0.2, true);
 % [objF, xopt]  = topOpt.solve();
 % toc
-
 
 % tic
 % topOpt = StressIntensityTopologyOptimizationVol( Rfilter, analysis, cutTreshold, penal, 0.42, true );
