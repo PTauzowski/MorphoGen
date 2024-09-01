169ce67cd27c7740264c20b03b4301cd4ee10249
piotrek <nabucco33@tlen.pl>
2024-09-01 21:33:07 +0200

    manipulator topologies

--- examples/topologyOpt/ManipulatorArm/ManipulatorArmWithBuckling.m
+++ examples/topologyOpt/ManipulatorArm/ManipulatorArmWithBuckling.m
@@ -9,5 +9,5 @@ close all;
 % ArmTopOptBucklingFn('Ms', 0.0E7, 0.0E7, 0.0E7, 0.0E7, 0.0E7, 1.0E7);
 
 ArmTopOptBucklingFn('My-Ms', 0.0E7, 0.0E7, 0.0E7, 0.0E7, 1.0E7, 1.0E7); 
-ArmTopOptBucklingFn('My-N',  0.0E7, 0.0E7, 1.0E7, 0.0E7, 1.0E7, 0.0E7); 
-ArmTopOptBucklingFn('Ms-N',  0.0E7, 0.0E7, 1.0E7, 0.0E7, 0.0E7, 1.0E7); 
+ArmTopOptBucklingFn('My-N',  0.0E7, 0.0E7, 1.0E7, 0.0E7, 2.0E7, 0.0E7); 
+ArmTopOptBucklingFn('Ms-N',  0.0E7, 0.0E7, 1.0E7, 0.0E7, 0.0E7, 2.0E7); 
