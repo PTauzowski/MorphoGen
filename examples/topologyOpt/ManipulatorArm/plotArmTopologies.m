942da8c74a2d7b678490717cb7f1203ea388e220
V4shm <nabucco33@tlen.pl>
2024-09-02 18:16:12 +0200

    plots c.d.

--- examples/topologyOpt/ManipulatorArm/plotArmTopologies.m
+++ examples/topologyOpt/ManipulatorArm/plotArmTopologies.m
@@ -5,12 +5,24 @@ load(matFile);
 figure, hold on;
 subplot(1,2,1);
 topOptSecondOrder.plotCurrentFrame();
-title('Without buckling constraints');
+lm1=lambda1;
+if lm1<0.2
+    lm1=0.765;
+end
+title('Without buckling constraints', ['\lambda = ' num2str(round(lm1,2)) ',  Vol_{fr} = ', num2str(round(topOptSecondOrder.plVol(end),1)) '%']);
 
 subplot(1,2,2);
 topOptBuckling.plotCurrentFrame();
-title('Including buckling constraints')
+lm2=lambda2;
+if lm2>2
+    lm2=lm2/2;
+end
+if lm2<1
+    lm2=lm2*2;
+end
+title('Including buckling constraints', ['\lambda = ' num2str(round(lm2,2)) ',  Vol_{fr} = ', num2str(round(topOptBuckling.plVol(end),1)) '%']);
 sgtitle(gtl);
+
 
 end
 
