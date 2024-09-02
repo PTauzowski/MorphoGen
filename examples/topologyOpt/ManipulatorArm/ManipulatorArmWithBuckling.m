5ca31d805e2289a399a6f7b02652c0fb9b461dfa
V4shm <nabucco33@tlen.pl>
2024-09-02 14:04:42 +0200

    plot c.d.

--- examples/topologyOpt/ManipulatorArm/ManipulatorArmWithBuckling.m
+++ examples/topologyOpt/ManipulatorArm/ManipulatorArmWithBuckling.m
@@ -19,6 +19,10 @@ close all;
 plotArmTopologies('ArmTopopt_N.mat','Normal force');
 plotArmTopologies('ArmTopopt_Tx.mat','Shear force along X direction');
 plotArmTopologies('ArmTopopt_Ty.mat','Shear force along Y direction');
-plotArmTopologies('ArmTopopt_Mx.mat','Bending moment along X direction');
-plotArmTopologies('ArmTopopt_My.mat','Bending moment along Y direction');
+plotArmTopologies('ArmTopopt_Mx.mat','Bending moment along X axis');
+plotArmTopologies('ArmTopopt_My.mat','Bending moment along Y axis');
 plotArmTopologies('ArmTopopt_Ms.mat','Torsion moment');
+
+plotArmTopologies('ArmTopopt_Ms-N.mat','Mixed mode Torsion and normalforce');
+plotArmTopologies('ArmTopopt_My-Ms.mat','Mixed mode Y asis bending and Torsion');
+plotArmTopologies('ArmTopopt_My-N.mat','Mixed mode Y asis bending and normal force');
