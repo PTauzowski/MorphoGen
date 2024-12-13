9b4ca0c8818e10cc2b7d8665cb9dae0c7e3a6190
piotrek <nabucco33@tlen.pl>
2024-12-13 07:07:26 +0100

    Top Opt  arm first examples

---
+++ examples/topologyOpt/ManipulatorArm/plotArmTopOptConfig.m
@@ -0,0 +1,13 @@
+function plotArmTopOptConfig(Rfilter, analysis, xopt, cutTreshold, penal, false)
+    figure;
+    topOpt = StressIntensityTopologyOptimizationVol( Rfilter, analysis, cutTreshold, penal, 0.4, false );
+    xtop_full=xopt;
+    nArms=size(analysis.mesh.elems,1)/size(xopt,1);
+    for k=1:nArms-1
+        xtop_full=[xtop_full; xopt; flip(xopt)];
+    end
+    xtop_full=[xtop_full; flip(xopt)];
+    topOpt.x=xtop_full;
+    topOpt.plotCurrentFrame();
+end
+
